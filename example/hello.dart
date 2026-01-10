45908f856be795ee1d7a8acb14c4f38b46c67fee
Tasrif <tasrifnasa@gmail.com>
2026-01-10 18:01:32 +0800

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,11 @@
+import 'package:belajar_dart_library/hello.dart';
+
+void main() {
+  print(sayHello("Tasrif"));
+
+  var customer = Customer();
+  var category = Category();
+  var product = Product();
+
+
+}
